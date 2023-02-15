efaf79bc23386828cee6d5e8aa1f93fea5664dbb
이하나 <lhn8118@naver.com>
2023-02-15 17:42:39 +0900

    'typescriptSlice'

--- src/Slice/InfoSlice.tsx
+++ src/Slice/InfoSlice.tsx
@@ -1,26 +1,36 @@
-import { createSlice, createAsyncThunk, Dispatch } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios';
 
 const API_URL = '';
+
+class ErrorClass extends Error {
+	response?: {
+		data: any;
+		status: number;
+		headers: string;
+	};
+}
 
 interface info {
 	[key: string]: string | number
 }
 
 interface initialState {
-	data: unknown,
+	data: info | null,
 	loading: boolean,
-	error: unknown | null
+	error: ErrorClass | null
 }
 
-export const getInfo = createAsyncThunk<info, info, {dispatch: Dispatch, state: unknown, extra: {jwt: string}, rejectValue: unknown }>('InfoSlice/getInfo', async (payload, { rejectWithValue }) => {
+export const getInfo = createAsyncThunk<info, info, {extra: {jwt: string}, rejectValue: (ErrorClass) }>('InfoSlice/getInfo', async (payload, { rejectWithValue }) => {
 	let result = null;
 
 	try {
 		const response = await axios.get(API_URL);
 		result = response.data;
 	} catch (err) {
-		result = rejectWithValue(err);
+		if(err instanceof ErrorClass) {
+			result = rejectWithValue(err);
+		}
 	}
 
 	return result;
@@ -42,7 +52,9 @@ const InfoSlice = createSlice({
 			state.data = payload;
 		}).addCase(getInfo.rejected, (state, {payload}) => {
 			state.loading = false;
-			state.error = payload;
+			if(typeof payload !== 'undefined') {
+				state.error = payload;
+			}
 		});
 	}
 });
